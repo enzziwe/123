637883beae958e3fe4bb50fbc0edf9eebf243863
Alexander Zavazzkii <60303860+enzziwe@users.noreply.github.com>
2022-06-04 10:36:26 +0500

    Add files via upload

---
+++ 121321/control_banana/script.js
@@ -0,0 +1,186 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+a1 = getRandomIntInclusive(1,2)
+a2 = getRandomIntInclusive(1,2)
+a3 = getRandomIntInclusive(1,2)
+a4 = getRandomIntInclusive(1,2)
+a5 = getRandomIntInclusive(1,2)
+a6 = getRandomIntInclusive(1,2)
+a7 = getRandomIntInclusive(1,2)
+a8 = getRandomIntInclusive(1,2)
+a9 = getRandomIntInclusive(1,2)
+a10 = getRandomIntInclusive(1,2)
+a11 = getRandomIntInclusive(1,2)
+a12 = getRandomIntInclusive(1,2)
+a13 = getRandomIntInclusive(1,2)
+a14 = getRandomIntInclusive(1,2)
+a15 = getRandomIntInclusive(1,2)
+a16 = getRandomIntInclusive(1,2)
+a17 = getRandomIntInclusive(1,2)
+a18 = getRandomIntInclusive(1,2)
+
+
+$("#b1").click(function () {
+    if (a1 == 1) {$("#b1").css({"color": "black","border": "white"}) 
+    } else {
+        $("#b1").css({"border": "white"
+        }) 
+    }
+})
+$("#b2").click(function () {
+    if (a2 == 1) {
+        $("#b2").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b2").css({"border": "white"
+        }) 
+    }
+})
+$("#b3").click(function () {
+    if (a3 == 1) {
+        $("#b3").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b3").css({"border": "white"
+        }) 
+    }
+})
+$("#b4").click(function () {
+    if (a4 == 1) {
+        $("#b4").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b4").css({"border": "white"
+        }) 
+    }
+})
+$("#b5").click(function () {
+    if (a5 == 1) {
+        $("#b5").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b5").css({"border": "white"
+        }) 
+    }
+})
+$("#b6").click(function () {
+    if (a6 == 1) {
+        $("#b6").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b6").css({"border": "white"
+        }) 
+    }
+})
+$("#b7").click(function () {
+    if (a7 == 1) {
+        $("#b7").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b7").css({"border": "white"
+        }) 
+    }
+})
+$("#b8").click(function () {
+    if (a8 == 1) {
+        $("#b8").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b8").css({"border": "white"
+        }) 
+    }
+})
+$("#b9").click(function () {
+    if (a9 == 1) {
+        $("#b9").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b9").css({"border": "white"
+        }) 
+    }
+})
+$("#b10").click(function () {
+    if (a10 == 1) {
+        $("#b10").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b10").css({"border": "white"
+        }) 
+    }
+})
+$("#b11").click(function () {
+    if (a11 == 1) {
+        $("#b11").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b11").css({"border": "white"
+        }) 
+    }
+})
+$("#b12").click(function () {
+    if (a12 == 1) {
+        $("#b12").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b12").css({"border": "white"
+        }) 
+    }
+})
+$("#b13").click(function () {
+    if (a13 == 1) {
+        $("#b13").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b13").css({"border": "white"
+        }) 
+    }
+})
+$("#b14").click(function () {
+    if (a14 == 1) {
+        $("#b14").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b14").css({"border": "white"
+        }) 
+    }
+})
+$("#b15").click(function () {
+    if (a15 == 1) {
+        $("#b15").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b15").css({"border": "white"
+        }) 
+    }
+})
+$("#b16").click(function () {
+    if (a16 == 1) {
+        $("#b16").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b16").css({"border": "white"
+        }) 
+    }
+})
+$("#b17").click(function () {
+    if (a17 == 1) {
+        $("#b17").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b17").css({"border": "white"
+        }) 
+    }
+})
+$("#b18").click(function () {
+    if (a18 == 1) {
+        $("#b18").css({"color": "black","border": "white"
+        }) 
+    } else {
+        $("#b18").css({"border": "white"
+        }) 
+    }
+})
